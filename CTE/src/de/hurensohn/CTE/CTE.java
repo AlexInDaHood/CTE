22d887f8c97490d188b24fce881f1280cf67b5b4
Admin <Admin@PC-Noah.fritz.box>
2022-02-21 15:54:06 +0100

    UwU-Chan

---
+++ CTE/src/de/hurensohn/CTE/CTE.java
@@ -0,0 +1,22 @@
+package de.hurensohn.CTE;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CTE extends JavaPlugin{
+	
+	public static CTE INSTANCE;
+	public static String prefix = "§8[§dCTE§8] §d";
+	
+	
+	@Override
+	public void onEnable() {
+		INSTANCE = this;
+		
+		
+	}
+	
+	@Override
+	public void onDisable() {
+	}
+	
+}
